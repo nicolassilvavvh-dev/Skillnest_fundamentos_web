43486398f680bc2ab71017a65c6882dd63d805fe
nicolassilvavvh-dev <nicolassilva@liceovvh.cl>
2026-05-25 16:09:55 -0400

    sams

--- STACK_JS/02_condiciones/static/js/script.js
+++ STACK_JS/02_condiciones/static/js/script.js
@@ -1,11 +1,105 @@
-console.log("Conexión de js exítosa...");
-function cambiarTexto() {
-            let titulo = "Hola, bienvenido";
-            if (document.getElementById("titulo").innerText == titulo){
-                document.getElementById("titulo").innerText = "Has cambiado el texto con JS";
-                document.getElementById("titulo").style.color= "red";
-        } else{
-            document.getElementById("titulo").innerText = titulo;
-            document.getElementById("titulo").style.color= "black";
-            }
+// ✅ Ejercicio 1: Sistema de descuento
+// 📝 Enunciado
+// Una tienda realiza descuentos según el monto de compra:
+// Si la compra es mayor o igual a $50.000 → 20% descuento
+// Si la compra es mayor o igual a $30.000 → 10% descuento
+// Si la compra es menor a $30.000 → sin descuento
+// El programa debe solicitar:
+// Nombre del cliente
+// Monto de compra
+// Luego mostrar:
+// Descuento aplicado
+// Total a pagar
+
+function ejercicio1(){
+    let nombre = prompt("Ingrese su nombre");
+    let monto = parseInt(prompt("Ingrese el monto de compra"))
+    if (monto >= 50000){
+        alert(`el monto con descuento es: ${monto - (monto * 0.20)}`)
+    } else if(monto >30000){
+        alert(`el monto con descuento es: ${monto - (monto * 0.10)}`)
+    } else if (monto <= 30000 && monto > 0){
+        alert(`el monto final es: ${monto}, sin descuento`)
+    } else{
+        alert(`Ingrese valores estimados`)
+    }
+}
+// ✅ Ejercicio 2: Acceso a evento
+// 📝 Enunciado
+// Una persona puede ingresar a un evento si:
+// Tiene 18 años o más
+// O viene acompañada por un adulto
+// Solicitar:
+// Edad
+// Si viene acompañado (si o no)
+// Mostrar:
+// “Acceso permitido”
+// o “Acceso denegado”
+function ejercicio2(){
+    let edad = prompt("Ingrese su edad");;
+    if (edad > 18){
+        alert(`Acceso permitido`)
+    } else if(edad < 18 && edad > 0){
+        let permitido = prompt("¿Viene con acompañante?: si/no ")
+        if (permitido == si){
+            alert("acceso permitido")
+        }else if(permitido == no){
+            alert("acceso denegado")
+        }else{
+            alert("ingrese un valor valido")
         }
+    }else{
+        alert("ingrese valores validos")
+    }
+}
+// ✅ Ejercicio 3: Clasificación de temperatura
+// 📝 Enunciado
+// Solicitar una temperatura y clasificar:
+// Menor a 10 → “Hace frío”
+// Entre 10 y 24 → “Temperatura agradable”
+// 25 o más → “Hace calor”
+// Además:
+// Si la temperatura supera los 35 grados, mostrar una alerta adicional:
+//  “Temperatura extrema”.
+function ejercicio3(){
+    let temperatura = prompt("ingrese la tempatura")
+    if(temperatura <= 10){
+        alert(`Hace frío`);
+    }else if(temperatura > 10 && temperatura <= 24){
+        alert(`Temperatura agradable`);
+    }else if(temperatura >= 25){
+        alert(`Hace calor`);
+    }else if(temperatura >= 35){
+        alert(`Temperatura extrema`)
+    }else{
+        alert(`ingresar temperatura valida`)
+    }
+}
+// ✅ Ejercicio 4: Verificación de administrador
+// 📝 Enunciado
+// Solicitar:
+// Nombre de usuario
+// Contraseña
+// Condiciones:
+// Usuario correcto: admin
+// Contraseña correcta: 12345
+// Casos:
+// Si ambos son correctos → “Bienvenido administrador”
+// Si usuario es correcto pero contraseña incorrecta → “Contraseña incorrecta”
+// Si usuario no existe → “Usuario no encontrado”
+
+// ✅ Ejercicio 5: Sistema de aprobación
+// 📝 Enunciado
+// Solicitar:
+// Nombre del estudiante
+// Nota 1
+// Nota 2
+// Nota 3
+// Calcular promedio.
+// Condiciones:
+// Promedio 6.0 o superior → “Aprobado con excelencia”
+// Promedio entre 4.0 y 5.9 → “Aprobado”
+// Menor a 4.0 → “Reprobado”
+// Además:
+// Si alguna nota es menor a 2.0 mostrar:
+// “Debe asistir a reforzamiento”.
