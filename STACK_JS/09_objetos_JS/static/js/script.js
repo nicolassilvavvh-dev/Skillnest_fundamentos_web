01aaf97bfffb58fc25b532af0018b4248baff579
nicolassilvavvh-dev <nicolassilva@liceovvh.cl>
2026-07-27 08:56:45 -0400

    Papaya

--- STACK_JS/09_objetos_JS/static/js/script.js
+++ STACK_JS/09_objetos_JS/static/js/script.js
@@ -1,6 +1,6 @@
 console.log("prueba de conexion");
-
-let hamburguesaEspecial = {
+function pan(){
+    let hamburguesaEspecial = {
     pan:"Pan brioche",
     carne:"Pollo crujiente",
     queso:"Suizo",
@@ -10,6 +10,7 @@ let hamburguesaEspecial = {
         "Miel"
     ],
     mostrarIngredientes:function(){
+        alert("Pan:" + this.pan)
         console.log("Pan:",this.pan);
         console.log("Carne:",this.carne);
         console.log("Queso:",this.queso);
@@ -19,34 +20,41 @@ let hamburguesaEspecial = {
         );
     }
 };
+}
 
 //Acceder al pan
 console.log(hamburguesaEspecial.pan);
 hamburguesaEspecial.mostrarIngredientes();
 
 //objetos con metodos automovil
+function metodoAuto() {
     let auto = {
     marca:"Toyota",
     modelo:"Corolla",
     año:2023,
     encender:function(){
-        console.log(
-            "Encendiendo",
-            this.marca,
-            this.modelo
-        );
+        alert(`Encendiendo:
+            \nMarca: ${this.marca}
+            \nModelo: ${this.modelo}
+            \nAño: ${this.año}`);
     }
-};auto.encender();
+}
+auto.encender();
+}
 
 
 //objeto con metodo casa
+function metodoCasa() {
 let casa = {
     direccion:"Av. Siempre Viva 742",
     habitaciones:4,
     baños:2,
     mostrarInformacion:function(){
-        console.log(
-            this.direccion
-        );
+        alert(`direccion: ${this.direccion}
+            \nHabitaciones: ${this.habitaciones}
+            \nBaños: ${this.baños}
+        `);
     }
-}; casa.mostrarInformacion();
+};
+casa.mostrarInformacion();
+}
