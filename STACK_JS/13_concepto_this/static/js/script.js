20450973d69e0eefb16820edab36093d8cde4037
nicolassilvavvh-dev <nicolassilva@liceovvh.cl>
2026-08-10 14:04:53 -0400

    sa s

--- STACK_JS/13_concepto_this/static/js/script.js
+++ STACK_JS/13_concepto_this/static/js/script.js
@@ -9,3 +9,14 @@ boton.addEventListener("click", function () {
         this.innerText = "¡Texto cambiado!";
     }
 });
+
+// Seleccionamos todas las imágenes con la clase 'imagenEliminar'
+let imagenes = document.querySelectorAll(".imagenEliminar");
+
+// Agregamos un evento 'click' a cada imagen
+imagenes.forEach(function (imagen) {
+    imagen.addEventListener("click", function () {
+       // Eliminamos la imagen al hacer clic
+        this.remove();
+    });
+});
