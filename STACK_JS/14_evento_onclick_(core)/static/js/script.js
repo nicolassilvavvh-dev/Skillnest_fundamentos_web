7182a6f5246719f63059b5fe81fefe9a7523530f
nicolassilvavvh-dev <nicolassilva@liceovvh.cl>
2026-08-10 15:34:04 -0400

    sans

--- STACK_JS/14_evento_onclick_(core)/static/js/script.js
+++ STACK_JS/14_evento_onclick_(core)/static/js/script.js
@@ -1,1 +1,28 @@
 console.log("Conexión de js exítosa...");
+// Seleccionamos el botón por su id
+let boton = document.getElementById("IniciarSesion");
+
+// Agregamos un evento 'click' al botón
+boton.addEventListener("click", function () {
+    if (this.innerText === "Cerrar sesion") {
+        this.innerText = "Iniciar sesion";
+    } else {
+        this.innerText = "Cerrar sesion";
+    }
+});
+
+let boton2 = document.getElementById("verPerfil");
+
+boton2.addEventListener("click", function () {
+    alert("Cargando el perfil de usuario...");
+});
+
+let botonesLikes = document.querySelectorAll(".btn-gusta");
+
+botonesLikes.forEach(function (boton) {
+    boton.addEventListener("click", function () {
+        let contador = parseInt(this.innerText);
+        contador++;
+        this.innerText = contador + " Me gustas";
+    });
+});
